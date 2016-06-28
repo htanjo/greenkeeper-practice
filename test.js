4eaaf3d27f077f8bddbd26201e13851c67393292
Hiroyuki Tanjo <expheno@gmail.com>
2016-06-28 13:14:45 +0900

    Create test

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const moduleName = require('.');
+const expect = require('chai').expect;
+
+describe('<moduleName>', () => {
+  it('returns true', () => {
+    expect(moduleName).to.be.true;
+  });
+});
